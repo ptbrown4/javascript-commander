759cfa378fdd8c71af94d51cdb59881f4931c316
Terry Brown <ptbrown514@gmail.com>
2018-10-25 22:13:26 -0500

    fixed workout

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ program
     .option('-c, --codingHomework', 'Finish Coding Homework')
     .option('-p, --pickUpHouse', 'Pick up the House')
     .option('-t, --takeNap', 'Take a nap')
-    .option('-w, --workout[type]', 'Workout', 'Crossfit')
+    .option('-w, --workout', 'Workout at Crossfit')
     .parse(process.argv);
  
 console.log('To Do List');
     if (program.codingHomework) console.log('  - Finish Coding HW');
     if (program.pickUpHouse) console.log('  - Clean the House');
     if (program.takeNap) console.log('  - Take a nap');
-console.log('  - %s workout', program.workout);
+    if (program.workout) console.log('  - Workout at Crossfit');
